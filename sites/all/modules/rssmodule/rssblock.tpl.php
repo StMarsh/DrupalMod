c64b278fcf7b289219552e10950dc1f7cdba013c
USER <marsh@ubuntu.(none)>
2014-02-12 15:54:47 +0200

    Done

---
+++ sites/all/modules/rssmodule/rssblock.tpl.php
@@ -0,0 +1,7 @@
+<?php
+echo '<h3>Latest 10 news:</h3>';
+foreach ($item as $items) {
+    echo "<h4><a href='". $items['link']. "'>". $items['title']. "</a></h4>";
+    echo "<p>". $items['description']. "</p>";
+}
+?>
